3602ef13b2d538e7156a18ab61f59c23c47e952e
roberto <roberto.marabini.uam.es>
2016-11-17 02:11:50 +0100

    add thread to connect example

--- client/connect.py
+++ client/connect.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 import urllib
 import urllib2
+import threading
 #hashlib.sha256()
-url='http://localhost:8000/report_protocols/api/workflow/workflow/addOrUpdateWorkflow/'
-opener = urllib2.build_opener(urllib2.HTTPHandler(debuglevel=1))
-data = urllib.urlencode({'hash':"myhash",
-                         'json':"myjson"})
-content = opener.open(url,data=data).read()
-print content
+#url='http://localhost:8000/'
+def connect():
+    url='https://secret-reaches-65198.herokuapp.com/'
+    webserver = 'report_protocols/api/workflow/workflow/addOrUpdateWorkflow/'
+    opener = urllib2.build_opener(urllib2.HTTPHandler(debuglevel=1))
+    data = urllib.urlencode({'hash':"myhash",
+                             'json':"myjson"})
+    content = opener.open(url+ webserver, data=data).read()
+    print content
+
+t = threading.Thread(target=connect)
+t.start() # will execute func in a separate thread
+
+#t.join() # will wait for spawned thread to die
