431d724107492c97fdf6129a9736adc6876c3db8
roberto <roberto.marabini.uam.es>
2016-11-20 12:35:52 +0100

    script to parse dataabse url

---
+++ utilities/database_parser.py
@@ -0,0 +1,12 @@
+import dj_database_url
+
+D = dj_database_url.config()
+print """export PGUSER=%s
+export PGPASSWORD=%s
+export PGHOST=%s
+export PGDATABASE=%s
+"""%(D['USER'],
+     D['PASSWORD'],
+     D['HOST'],
+     D['NAME'])
+
