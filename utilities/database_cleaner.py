c65ccbfe3c62c44bdf076dbf3305481e9b7dfff0
roberto <roberto.marabini.uam.es>
2016-11-20 12:57:15 +0100

    rename database_parser.py

--- utilities/database_cleaner.py
+++ utilities/database_cleaner.py
@@ -13,15 +13,46 @@ export PGDATABASE=%s
      D['NAME'])
 
 #drop tables
-print """
-DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO public;
-COMMENT ON SCHEMA public IS 'standard public schema';
-"""
-print """
-select 'drop table "' || tablename || '" cascade;'
-from pg_tables where schemaname = 'public';
-"""
+###print """
+#DROP SCHEMA public CASCADE;
+#CREATE SCHEMA public;
+#GRANT ALL ON SCHEMA public TO postgres;
+#GRANT ALL ON SCHEMA public TO public;
+#COMMENT ON SCHEMA public IS 'standard public schema';
+#"""
+#print """
+#select 'drop table "' || tablename || '" cascade;'
+#from pg_tables where schemaname = 'public';
+#"""
 ###select 'drop table "' || tablename || '" cascade;' from pg_tables;
+import psycopg2
+import sys
+
+# Drop all tables from a given database
+
+try:
+    conn = psycopg2.connect("""dbname='%s'
+                                 user='%s'
+                             password='%s'
+                                 host='%s'"""%(D['NAME'],
+                                               D['USER'],
+                                               D['PASSWORD'],
+                                               D['HOST']
+                                               )
+                            )
+    conn.set_isolation_level(0)
+except:
+    print "Unable to connect to the database."
+
+cur = conn.cursor()
+
+try:
+    cur.execute("SELECT table_schema,table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_schema,table_name")
+    rows = cur.fetchall()
+    for row in rows:
+        print "dropping table: ", row[1]
+        cur.execute("drop table " + row[1] + " cascade")
+    cur.close()
+    conn.close()
+except:
+    print "Error: ", sys.exc_info()[1]
