b9413a0e214b519908fda071dc19ecc192b51782
roberto <roberto.marabini.uam.es>
2016-11-20 12:41:39 +0100

    script to parse dataabse url

--- utilities/database_parser.py
+++ utilities/database_parser.py
@@ -1,6 +1,8 @@
 import dj_database_url
 
 D = dj_database_url.config()
+
+#set databae variables
 print """export PGUSER=%s
 export PGPASSWORD=%s
 export PGHOST=%s
@@ -10,3 +12,16 @@ export PGDATABASE=%s
      D['HOST'],
      D['NAME'])
 
+#drop tables
+print """
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO public;
+COMMENT ON SCHEMA public IS 'standard public schema';
+"""
+print """
+select 'drop table "' || tablename || '" cascade;'
+from pg_tables where schemaname = 'public';
+"""
+###select 'drop table "' || tablename || '" cascade;' from pg_tables;
