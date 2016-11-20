4bd3fc4a3a640d0fdc0ea57f1375ee9aa70fc2c0
roberto <roberto.marabini.uam.es>
2016-11-20 20:51:17 +0100

    remove comments

--- utilities/database_cleaner.py
+++ utilities/database_cleaner.py
@@ -2,7 +2,7 @@ import dj_database_url
 
 D = dj_database_url.config()
 
-#set databae variables
+#set database variables
 print """export PGUSER=%s
 export PGPASSWORD=%s
 export PGHOST=%s
@@ -12,19 +12,6 @@ export PGDATABASE=%s
      D['HOST'],
      D['NAME'])
 
-#drop tables
-###print """
-#DROP SCHEMA public CASCADE;
-#CREATE SCHEMA public;
-#GRANT ALL ON SCHEMA public TO postgres;
-#GRANT ALL ON SCHEMA public TO public;
-#COMMENT ON SCHEMA public IS 'standard public schema';
-#"""
-#print """
-#select 'drop table "' || tablename || '" cascade;'
-#from pg_tables where schemaname = 'public';
-#"""
-###select 'drop table "' || tablename || '" cascade;' from pg_tables;
 import psycopg2
 import sys
 
