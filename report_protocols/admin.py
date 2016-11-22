51889f6f4e520702f88ca58ec5fa9abe04fb96f8
roberto <roberto.marabini.uam.es>
2016-11-22 16:33:38 +0100

    improve admin page visibility

--- report_protocols/admin.py
+++ report_protocols/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from report_protocols.models import Protocol, Workflow
 
-#class ProtocolAdmin(admin.ModelAdmin):
-#    list_display = ('name', 'timesUsed')
-#    ordering = ("-timesUsed")
+class WorkflowAdmin(admin.ModelAdmin):
+    list_display = ('project_uuid', 'date','client_ip','timesModified')
+    ordering = ("-date")
 
 # Register your models here.
 admin.site.register(Protocol)
-admin.site.register(Workflow)
+admin.site.register(WorkflowAdmin)
+
+
