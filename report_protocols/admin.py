73b59a787846701284838c5d6c466bc03851cf65
roberto <roberto.marabini.uam.es>
2016-11-22 16:58:24 +0100

    improve admin page visibility

--- report_protocols/admin.py
+++ report_protocols/admin.py
@@ -3,7 +3,7 @@ from report_protocols.models import Protocol, Workflow
 
 class WorkflowAdmin(admin.ModelAdmin):
     list_display = ('project_uuid', 'date','client_ip','timesModified')
-    ordering = ("date",)
+    ordering = ("-date",)
 
 # Register your models here.
 admin.site.register(Protocol)
