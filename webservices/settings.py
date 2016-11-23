a15a9d1c5c6bf9c875d83404de56cf005dfe32fc
roberto <roberto.marabini.uam.es>
2016-11-23 22:04:15 +0100

    now you can see the result without login

--- webservices/settings.py
+++ webservices/settings.py
@@ -125,12 +125,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles' )
-STATIC_PATH = os.path.join(BASE_DIR,'static')
 
 # Extra places for collectstatic to find static files.
+STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+
+STATIC_PATH = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = (
-    STATIC_PATH,'../static',
+    STATIC_PATH,
 )
+
