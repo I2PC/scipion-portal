960f9f93aeb4fd3d0932c6ec2701ff3565c4e3cd
roberto <roberto.marabini.uam.es>
2016-11-23 22:56:25 +0100

    now you can see the result without login

--- webservices/urls.py
+++ webservices/urls.py
@@ -19,11 +19,13 @@ from django.contrib import admin
 from django.conf.urls import include
 
 from report_protocols import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^report_protocols/', include('report_protocols.urls')),
     url(r'^ask_version/', include('ask_version.urls')),
     url(r'^admin/', admin.site.urls),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
