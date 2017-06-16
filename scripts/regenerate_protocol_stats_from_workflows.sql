fecfd537bafe5c8269ac50e75f51208cc6572c0c
pconesa <p.conesa.mingo@gmail.com>
2017-06-16 19:25:39 +0200

    add sql to regenerate protocol stats from workflows data

---
+++ scripts/regenerate_protocol_stats_from_workflows.sql
@@ -0,0 +1,14 @@
+﻿select protocol, count(*) from (
+select unnest(
+	string_to_array(
+	 replace(
+	  replace(
+	   replace(
+	    replace(project_workflow,', ',',')
+	   ,'"','')
+	  ,']','')
+	 ,'[','')
+        ,',')
+        ) as protocol
+from report_protocols_workflow) prot_list
+group by protocol
