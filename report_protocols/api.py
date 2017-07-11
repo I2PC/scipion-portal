0f992f545fb29642cfa0a2014c431a966ebaca78
rmarabini <roberto@cnb.csic.es>
2017-07-11 02:46:42 +0200

    force isInBlackList to return true always

--- report_protocols/api.py
+++ report_protocols/api.py
@@ -53,6 +53,7 @@ class WorkflowResource(ModelResource):
     def isInBlackList(self,ip):
         """ check if ip address is in blackList and then return False. 
             Otherwise return True"""
+        return True
         try:
             IpAddressBlackList.objects.get(client_ip=ip)
         except IpAddressBlackList.DoesNotExist:
