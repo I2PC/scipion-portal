c02bc6e51b9b70e3ffc326c2205360b6bded5990
roberto <roberto@cnb.csic.es>
2017-07-13 14:36:43 +0200

    add clean example

--- send_sms_boto_v2.py
+++ send_sms_boto_v2.py
@@ -3,14 +3,14 @@ import boto3
 # Create an SNS client
 client = boto3.client(
     "sns",
-    aws_access_key_id="AKIAIGXWMESOWUVPSKVA",
-    aws_secret_access_key="znrE8qhLJLw8wUfqeuY7o7dUWjS+eCqmm4w21lX/",
+    aws_access_key_id="",
+    aws_secret_access_key="",
     region_name="us-east-1"
 )
 
 # Send your sms message.
 response = client.publish(
-    PhoneNumber="+34600055805",
+    PhoneNumber="+34",
     Message="Hello World!"
 )
 print("Response: {}".format(response))
