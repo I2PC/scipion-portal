125d21dae1d9e26215b5bd6b20234889901372d2
roberto <roberto@cnb.csic.es>
2017-07-19 17:50:15 +0200

    make ip unique so josue does not get nasty ip errors

--- report_protocols/models.py
+++ report_protocols/models.py
@@ -13,7 +13,7 @@ class Protocol(models.Model):
         return "prot=%s, used=%d"%(self.name, self.timesUsed)
 
 class IpAddressBlackList(models.Model):
-    client_ip = models.GenericIPAddressField(null=True)
+    client_ip = models.GenericIPAddressField(null=True,unique=True)
     note = models.CharField(max_length=128, null=True)
     def __str__(self):
         return str(self.client_ip)
