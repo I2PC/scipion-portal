91021c46591653f5e02fc62cbc6cde9956a99bba
Arnau Sanchez <pyarnau@gmail.com>
2017-11-30 13:03:57 +0100

    Add download to admin panel

---
+++ web/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from models import Download
+
+class DownloadAdmin(admin.ModelAdmin):
+    list_display = ('fullName', 'organization', 'country', 'email', 'subscription', 'version', 'platform')
+    search_fields = ('fullName', 'organization', 'country', 'email', 'platform')
+    ordering = ("creation",)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return (False if obj else True)
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+admin.site.register(Download, DownloadAdmin)
