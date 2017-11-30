2c5cde659cfba0c45aea42390d51f33b6449e6b8
Arnau Sanchez <pyarnau@gmail.com>
2017-11-30 13:08:46 +0100

    Order admin/downloads by creation desc

--- web/admin.py
+++ web/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from models import Download
 
 class DownloadAdmin(admin.ModelAdmin):
-    list_display = ('fullName', 'organization', 'country', 'email', 'subscription', 'version', 'platform')
-    search_fields = ('fullName', 'organization', 'country', 'email', 'platform')
-    ordering = ("creation",)
+    list_display = ("fullName", "organization", "country", "subscription", "version", "platform", "creation")
+    search_fields = ("fullName", "organization", "country", "email", "platform")
+    ordering = ("-creation",)
 
     def has_add_permission(self, request):
         return False
